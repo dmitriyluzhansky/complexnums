64be9222c9519bca9c8e9413dfe7dcf8c56d750f
dmitriy.luzhansky <dmitriy.luzhansky@gmail.com>
2021-07-31 10:25:52 +0400

    вызов калькулятора

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 
-echo "hi";
+include 'ComplexsNumsCalc.php';
+
+$calc_result = ComplexsNumsCalc::complex_nums_calculation('23+96i','35-574i','plus');
+$calc_test_result = ComplexsNumsCalc::test_calc();
+
+echo '<pre>';
+print_r($calc_result);
+print_r($calc_test_result);
 
 
 
